1c2a736751c02860ff5aeb90169736acb4c1a1ad
Vasyl Mykhaylyshyn <vasyl.mykhaylyshyn@eleks.com>
2024-11-25 16:34:30 +0200

    Added permisions for jenkins user to work with docker

--- terraform/modules/jenkins/main.tf
+++ terraform/modules/jenkins/main.tf
@@ -113,6 +113,9 @@ curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/sha
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update -y && sudo apt-get install jenkins -y
 
+# Add jenkins user to Docker group
+sudo usermod -aG docker jenkins
+
 # Run services
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
